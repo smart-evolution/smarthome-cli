aa86db80bd65c5821a419f1693415ded1e086f56
Oskar Szura <oskar.szura@gmail.com>
2019-03-06 08:22:08 +0100

    HOME-94 - Write basic CLI

--- main.go
+++ main.go
@@ -3,27 +3,51 @@ package main
 import (
 	"fmt"
     "net"
+    "os"
 )
 
-const (
-	SERVICE_ADDR = "93.180.188.173:3333"
-)
+func msgConstructor(cmd string) []byte {
+    msgString :=  `{
+        "cmd": "` + cmd + `"
+    }`
+
+    return []byte(msgString)
+}
 
 func main() {
-    conn, err := net.Dial("tcp", SERVICE_ADDR)
+    conn, err := net.Dial("tcp", os.Getenv("SMARTHOME_CLI_SRV"))
 
     if err != nil {
-        fmt.Println(err)
-    }
-    // fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
-    // status, err := bufio.NewReader(conn).ReadString('\n')
-
-    msgBytes := []byte("----- some message")
-    l, err := conn.Write(msgBytes)
-
-    if err != nil {
-        fmt.Println(err)
+        fmt.Println("error connecting to the smarthome cli server")
+        os.Exit(1)
     }
 
-    fmt.Println(l)
+    var cmd string
+    if len(os.Args) > 1 {
+        cmd = os.Args[1]
+    } else {
+        cmd = ""
+    }
+
+    var response string
+    buff := make([]byte, 512)
+
+    switch cmd {
+    case "status":
+        msg := msgConstructor("status")
+        _, err = conn.Write(msg)
+        n, err := conn.Read(buff)
+
+        if err != nil {
+            fmt.Println("error reading cli message")
+
+        }
+
+        response = string(buff[:n])
+    default:
+        response = "Invalid command"
+    }
+
+
+    fmt.Println(response)
 }

803f609836d8abd3d434100d32887c1d1c231d3b
Oskar Szura <oskar.szura@gmail.com>
2019-04-02 21:08:17 +0200

    HOME-117 - Implement device connection protocol (#5)

--- commands/connect/connect.go
+++ commands/connect/connect.go
@@ -1,13 +1,26 @@
 package connect
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
+
+var comms = map[string]map[string][]string{
+	"jeep": map[string][]string{
+		"s": []string{"CMD010", "CMD020"},
+		"w": []string{"CMD010", "CMD020", "CMD012", "CMD022"},
+		"a": []string{"CMD010", "CMD020", "CMD012", "CMD122"},
+		"d": []string{"CMD010", "CMD020", "CMD112", "CMD022"},
+		"x": []string{"CMD010", "CMD020", "CMD112", "CMD122"},
+	},
+}
 
 func Handler() {
 	var device string
+
 	if len(os.Args) > 2 {
 		device = os.Args[2]
 	} else {
@@ -22,17 +35,10 @@ func Handler() {
 		os.Exit(1)
 	}
 
-	var cmd string
-	if len(os.Args) > 3 {
-		cmd = os.Args[3]
-	} else {
-		fmt.Println("device command required")
-		os.Exit(1)
-	}
+	_, err = conn.Write([]byte("CMDWHO"))
 
-	_, err = conn.Write([]byte(cmd))
 	if err != nil {
-		fmt.Println("error sending command " + cmd)
+		fmt.Println("error getting device type")
 		os.Exit(1)
 	}
 
@@ -40,9 +46,37 @@ func Handler() {
 	n, err := conn.Read(buff)
 
 	if err != nil {
-		fmt.Println("error reading message from device")
+		fmt.Println("error retrieving device type")
+		os.Exit(1)
 	}
 
-	response := string(buff[:n])
-	fmt.Println(response)
+	devType := string(buff[:n])
+
+	if _, ok := comms[devType]; !ok {
+		fmt.Println("unknown device type '" + devType + "'")
+		os.Exit(1)
+	}
+
+	fmt.Println("connected to device type '" + devType + "'")
+
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("CMD: ")
+		input, _ := reader.ReadString('\n')
+		cmd := strings.TrimSpace(input)
+
+		if cmd == "disconnect" {
+			conn.Close()
+			break
+		}
+
+		hardwareComms := comms[devType][cmd]
+
+		for _, c := range hardwareComms {
+			_, err = conn.Write([]byte(c))
+			if err != nil {
+				fmt.Println("RES: sending command failed " + c)
+			}
+		}
+	}
 }
---
+++ commands/send/send.go
@@ -0,0 +1,48 @@
+package send
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func Handler() {
+	var device string
+	if len(os.Args) > 2 {
+		device = os.Args[2]
+	} else {
+		fmt.Println("device address required")
+		os.Exit(1)
+	}
+
+	conn, err := net.Dial("tcp", device)
+
+	if err != nil {
+		fmt.Println("error connecting device " + device)
+		os.Exit(1)
+	}
+
+	var cmd string
+	if len(os.Args) > 3 {
+		cmd = os.Args[3]
+	} else {
+		fmt.Println("device command required")
+		os.Exit(1)
+	}
+
+	_, err = conn.Write([]byte(cmd))
+	if err != nil {
+		fmt.Println("error sending command " + cmd)
+		os.Exit(1)
+	}
+
+	buff := make([]byte, 512)
+	n, err := conn.Read(buff)
+
+	if err != nil {
+		fmt.Println("error reading message from device")
+	}
+
+	response := string(buff[:n])
+	fmt.Println(response)
+}

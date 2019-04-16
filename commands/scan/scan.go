d260f4978d404d2afd23bbfd82a078a317f5e16c
Oskar Szura <oskar.szura@gmail.com>
2019-04-16 22:20:49 +0200

    HOME-135 - Improve devices scanning

--- commands/scan/scan.go
+++ commands/scan/scan.go
@@ -4,39 +4,49 @@ import (
 	"fmt"
 	"net"
 	"strconv"
+	"sync"
 	"time"
 )
 
 var addrs []string
 
-func Handler() {
-	for i := 1; i <= 255; i++ {
-		fmt.Print("\033[H\033[2J")
-		addr := "192.168.1." + strconv.Itoa(i)
-		fmt.Println("checking " + strconv.Itoa(i) + " address out of 255")
+func scan(wg *sync.WaitGroup, ip string) {
+	defer wg.Done()
+	//		fmt.Print("\033[H\033[2J")
+	//		fmt.Println("checking " + strconv.Itoa(i) + " address out of 255")
 
-		d := net.Dialer{Timeout: time.Duration(500) * time.Millisecond}
-		conn, err := d.Dial("tcp", addr+":81")
-		if err != nil {
-			continue
-		}
-
-		_, err = conn.Write([]byte("CMDWHO"))
-
-		if err != nil {
-			continue
-		}
-
-		buff := make([]byte, 512)
-		n, err := conn.Read(buff)
-
-		if err != nil {
-			continue
-		}
-
-		devType := string(buff[:n])
-		addrs = append(addrs, addr+"::"+devType)
+	d := net.Dialer{Timeout: time.Duration(1000) * time.Millisecond}
+	conn, err := d.Dial("tcp", ip + ":81")
+	if err != nil {
+		return
 	}
+
+	_, err = conn.Write([]byte("CMDWHO"))
+
+	if err != nil {
+		return
+	}
+
+	buff := make([]byte, 512)
+	n, err := conn.Read(buff)
+
+	if err != nil {
+		return
+	}
+
+	devType := string(buff[:n])
+	addrs = append(addrs, ip+"::"+devType)
+}
+
+func Handler() {
+	var wg sync.WaitGroup
+
+	for i := 1; i <= 255; i++ {
+		ip := "192.168.1." + strconv.Itoa(i)
+		wg.Add(1)
+		go scan(&wg, ip)
+	}
+	wg.Wait()
 
 	fmt.Println(strconv.Itoa(len(addrs)) + " compliant devices found")
 	for _, dev := range addrs {
